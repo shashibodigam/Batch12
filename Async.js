da027a4d6835ba9b1c9f9f60fb67a46ebd404cf1
shashibodigam <158171924+shashibodigam@users.noreply.github.com>
2025-01-23 10:33:42 +0530

    Add files via upload

--- Async.js
+++ Async.js
@@ -16,29 +16,36 @@
 //     reject("Failures");
 // });
 // console.log(p3);
- function fetchUsers()
- {
-    let x = fetch("https://jsonplaceholder.typicode.com/users");
-    // console.log(x);
-    x
-    .then((response)=>{
-        // console.log(response)
-        // console.log(response.json());
-        return response.json().then(data=>{
-            console.log(data);
-            let store = document.getElementById("store");
-            data.map((user)=>{
-                store.innerHTML += 
-                <tr>
-                    <td>${user.id}</td>
-                    <td>${user.name}</td>
-                    <td>${user.email}</td>
-                    <td>${user.company.name}</td> 
-                </tr>
-            })
-        })
-    })
-    .catch(err=>console.log(err))
+//  function fetchUsers()
+//  {
+//     let x = fetch("https://jsonplaceholder.typicode.com/users");
+//     // console.log(x);
+//     x
+//     .then((response)=>{
+//         // console.log(response)
+//         // console.log(response.json());
+//         return response.json().then(data=>{
+//             console.log(data);
+//             let store = document.getElementById("store");
+//             data.map((user)=>{
+//                 store.innerHTML += 
+//                 <tr>
+//                     <td>${user.id}</td>
+//                     <td>${user.name}</td>
+//                     <td>${user.email}</td>
+//                     <td>${user.company.name}</td> 
+//                 </tr>
+//             })
+//         })
+//     })
+//     .catch(err=>console.log(err))
 
+// }
+// fetchUsers()
+async function fetchusers(){
+    let response = await fetch("https://api.github.com/users");
+    let data = await response.json();
+    console.log(data);
+    
 }
-fetchUsers();
+fetchusers();
---
+++ Dom.js
@@ -0,0 +1,7 @@
+let element = document.getElementById("demo");
+element.innerText  = "DOM";
+console.log(element);
+
+let div = document.getElementBtId("test");
+div.innertHTML = "<h1>Header/<h1>"
+console.log(div)
